aba6c6ce0cbd92186ed4b62b0d4afc3fe41abf8b
BoreaWolf <ricky.orizio@gmail.com>
2017-06-07 10:31:53 +0100

    Added a new network error to handle
    ( .. )

--- Updateroo.rb
+++ Updateroo.rb
@@ -30,7 +30,7 @@ def open( url_base, url_spec )
 			# with the specific data required
 			http.get( "#{uri_base.path}#{url_spec}" ).body
 		end
-	rescue Net::ReadTimeout, Net::HTTPFatalError, Errno::ECONNREFUSED => error
+	rescue Net::ReadTimeout, Net::HTTPFatalError, Errno::ECONNREFUSED, Errno::EHOSTUNREACH => error
 		puts "Network problem #{error.class}..Waiting before retrying to get information from #{url_base}#{url_spec}"
 		sleep( 60 )
 		retry
